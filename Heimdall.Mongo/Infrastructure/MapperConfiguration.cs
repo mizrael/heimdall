104512cc127bd8ef65df52c3f4921f08da2fe0eb
mizrael <mizrael@yahoo.it>
2017-03-26 22:32:50 +0200

    updated mapping

--- Heimdall.Mongo/Infrastructure/MapperConfiguration.cs
+++ Heimdall.Mongo/Infrastructure/MapperConfiguration.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Linq;
+
 namespace Heimdall.Mongo.Infrastructure
 {
     public static class MapperConfiguration
@@ -7,7 +8,19 @@ namespace Heimdall.Mongo.Infrastructure
         {
             cfg.CreateMap<Entities.ServiceEndpoint, Heimdall.Core.Queries.Models.ServiceEndpoint>();
             cfg.CreateMap<Entities.Service, Heimdall.Core.Queries.Models.ServiceDetails>();
-            cfg.CreateMap<Entities.Service, Heimdall.Core.Queries.Models.ServiceArchiveItem>();
+            cfg.CreateMap<Entities.Service, Heimdall.Core.Queries.Models.ServiceArchiveItem>()
+                .ForMember(e => e.EndpointsCount, mo => {
+                    mo.ResolveUsing((Entities.Service s) =>
+                    {
+                        return (null != s.Endpoints) ? s.Endpoints.Count(se => se.Active) : 0;
+                    });
+                })
+                .ForMember(e => e.RoundtripTime, mo => {
+                    mo.ResolveUsing((Entities.Service s) =>
+                    {
+                        return (null != s.Endpoints && s.Endpoints.Any(se => se.Active)) ? s.Endpoints.Where(se => se.Active).Min(se => se.RoundtripTime) : long.MaxValue;
+                    });
+                });
         }
     }
 }
