12b9862c646df4e740c3929517e4dbb02127072d
mizrael <mizrael@yahoo.it>
2017-04-07 18:20:02 +0200

    added parameter to force loading all details regardless the endpoints status

--- Heimdall.Core/Queries/FindService.cs
+++ Heimdall.Core/Queries/FindService.cs
@@ -6,12 +6,14 @@ namespace Heimdall.Core.Queries
 {
     public class FindService : IRequest<ServiceDetails>
     {
-        public FindService(string serviceName)
+        public FindService(string serviceName, bool forceLoad)
         {
             if (string.IsNullOrWhiteSpace(serviceName))
                 throw new ArgumentNullException(nameof(serviceName));
             this.ServiceName = serviceName;
+            this.ForceLoad = forceLoad;
         }
         public string ServiceName { get; private set; }
+        public bool ForceLoad { get; private set; }
     }
 }
