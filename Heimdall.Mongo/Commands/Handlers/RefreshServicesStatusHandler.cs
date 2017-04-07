af8c898045db3248b9dfcb2d24b18d2c26183748
mizrael <mizrael@yahoo.it>
2017-04-07 18:19:13 +0200

    updated to call single service refresh handler

--- Heimdall.Mongo/Commands/Handlers/RefreshServicesStatusHandler.cs
+++ Heimdall.Mongo/Commands/Handlers/RefreshServicesStatusHandler.cs
@@ -3,6 +3,7 @@ using Heimdall.Mongo.Infrastructure;
 using LibCore.CQRS.Commands.Handlers;
 using LibCore.CQRS.Validation;
 using LibCore.Web.Services;
+using MediatR;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,13 +12,13 @@ namespace Heimdall.Mongo.Commands.Handlers
 {
     public class RefreshServicesStatusHandler : BaseCommandHandler<RefreshServicesStatus>
     {
-        private IDbContext _db;
-        private IPinger _pinger;
-
-        public RefreshServicesStatusHandler(IDbContext db, IPinger pinger, IValidator<RefreshServicesStatus> validator) : base(validator)
+        private readonly IMediator _mediator;
+        private readonly IDbContext _db;
+        
+        public RefreshServicesStatusHandler(IDbContext db, IMediator mediator, IValidator<RefreshServicesStatus> validator) : base(validator)
         {
             _db = db ?? throw new ArgumentNullException(nameof(db));
-            _pinger = pinger ?? throw new ArgumentNullException(nameof(pinger));
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
         }
 
         protected override async Task RunCommand(RefreshServicesStatus command)
@@ -28,18 +29,7 @@ namespace Heimdall.Mongo.Commands.Handlers
 
             foreach (var service in services)
             {
-                service.Active = false;
-
-                if (null != service.Endpoints && service.Endpoints.Any())
-                    foreach (var endpoint in service.Endpoints)
-                    {
-                        var pingResult = await _pinger.PingAsync(endpoint.Url, command.Timeout);
-                        service.Active |= pingResult.Success;
-                        endpoint.Active = pingResult.Success;
-                        endpoint.RoundtripTime = pingResult.RoundtripTime;
-                    }
-
-                await _db.Services.UpsertOneAsync(s => s.Id == service.Id, service);
+                await _mediator.Publish(new RefreshServiceStatus(service.Name, command.Timeout));
             }
             
         }
